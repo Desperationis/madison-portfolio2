9b8bd5844bc22dd299a87429e4533932096df41d
Desperationis <fork_important@pm.me>
2026-01-02 15:27:08 -0500

    Fix sketch naming bug

--- http_gen.py
+++ http_gen.py
@@ -147,12 +147,12 @@ class IndexPage:
 
 class CategoryPage:
     def __init__(self, category, cwd):
-        self.header = """<!DOCTYPE html>
+        self.header = f"""<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Madison Luna - Sketches</title>
+  <title>Madison Luna - {category.name}</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&family=Playfair+Display:wght@600&display=swap" rel="stylesheet">
